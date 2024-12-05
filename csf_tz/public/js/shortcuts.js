ef4ca5d396e4024aec96cc7dd7d4ff4bff0251ef
iphenelist <innocentphenelist@gmail.com>
2024-12-05 10:28:52 +0300

    fix: Ctrl+I to select customer item price is on SO

--- csf_tz/public/js/shortcuts.js
+++ csf_tz/public/js/shortcuts.js
@@ -84,63 +84,82 @@ function ctrlQ (TableName) {
     });
 }
 
-function ctrlI (TableName) {
+function ctrlI(TableName) {
+    // Get the current document details
     const current_doc = $('.data-row.editable-row').parent().attr("data-name");
     const item_row = locals[TableName][current_doc];
-    new frappe.ui.form.SelectDialog({
-        target: cur_frm,
-        title: "Select The Rate",
-        multi_select: 0,
-        date_field: "posting_date",
-        query_fields: [
-            {
-                fieldname: "rate",
-                fieldtype: "Currency",
-                label: "Rate",
-                options: "currency",
-                precision: "2",
-                filter: 0
-            },
-            {
-                fieldname: "qty",
-                fieldtype: "Float",
-                label: "Qty",
-                filter: 0
-            },
-            {
-                fieldname: "invoice",
-                fieldtype: "Link",
-                label: "Invoice",
-                options: "Sales Invoice",
-                filter: 0
-            },
-            {
-                default: cur_frm.doc.customer,
-                fieldname: "customer",
-                fieldtype: "Link",
-                label: "Customer",
-                options: "Customer",
-                filter: 1
-            },
 
-        ],
-        get_query () {
-            return {
-                filters: {
-                    item_code: item_row.item_code,
-                    customer: "",
-                    currency: cur_frm.doc.currency,
-                    company: cur_frm.doc.company
-                },
-                query: "csf_tz.custom_api.get_item_prices_custom",
-            };
-        },
-        return_field: "rate",
-        action (selections) {
-            console.log(selections);
-            frappe.model.set_value(item_row.doctype, item_row.name, 'rate', selections[0]);
-            cur_frm.refresh_fields();
+    // Prepare filters for the query
+    const filters = {
+        item_code: item_row.item_code,
+        customer: cur_frm.doc.customer || "",
+        currency: cur_frm.doc.currency,
+        company: cur_frm.doc.company
+    };
 
+    // Call the custom API to fetch data
+    frappe.call({
+        method: "csf_tz.custom_api.get_item_prices_custom",
+        args: { filters: filters },
+        callback: function (response) {
+            if (response.message && response.message.length > 0) {
+                const e = new frappe.ui.Dialog({
+                    title: __('Item Prices'),
+                    width: 600
+                });
+
+                $(`<div class="modal-body ui-front">
+                            <h2>${item_row.item_code} : ${item_row.qty}</h2>
+                            <p>Choose Price and click Select :</p>
+                            <table class="table table-bordered">
+                            <thead>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                            </table>
+                        </div>`).appendTo(e.body);
+
+                const thead = $(e.body).find('thead');
+                $(`<tr>
+                            <th>Check</th>
+                            <th>Rate</th>
+                            <th>Qty</th>
+                            <th>Date</th>
+                            <th>Invoice</th>
+                            <th>Customer</th>
+                        </tr>`).appendTo(thead);
+
+                response.message.forEach(element => {
+                    const tbody = $(e.body).find('tbody');
+                    const tr = $(`
+                            <tr>
+                                <td><input type="checkbox" class="check-rate" data-rate="${element.rate}"></td>
+                                <td>${element.rate}</td>
+                                <td>${element.qty}</td>
+                                <td>${element.posting_date}</td>
+                                <td>${element.invoice}</td>
+                                <td>${element.customer}</td>
+                            </tr>
+                            `).appendTo(tbody);
+
+                    tbody.find('.check-rate').on('change', function () {
+                        $('input.check-rate').not(this).prop('checked', false);
+                    });
+                });
+
+                e.set_primary_action("Select", function () {
+                    $(e.body).find('input:checked').each(function (i, input) {
+                        frappe.model.set_value(item_row.doctype, item_row.name, 'rate', $(input).attr('data-rate'));
+                    });
+                    cur_frm.rec_dialog.hide();
+                    cur_frm.refresh_fields();
+                });
+
+                cur_frm.rec_dialog = e;
+                e.show();
+            } else {
+                frappe.show_alert({ message: __('There are no records'), indicator: 'red' }, 5);
+            }
         }
     });
 }

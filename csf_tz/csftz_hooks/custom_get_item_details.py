b3570b7c00c123d1fa0c0e18c498d67ca5e1d684
iamtalib13 <talibsheikh16@gmail.com>
2024-12-26 17:06:32 +0530

    fix: resolve override of get_item_details function

--- csf_tz/csftz_hooks/custom_get_item_details.py
+++ csf_tz/csftz_hooks/custom_get_item_details.py
@@ -8,11 +8,11 @@ from erpnext.stock.get_item_details import get_item_details
 @frappe.whitelist()
 def custom_get_item_details(args, doc=None, for_validate=False, overwrite_warehouse=True):
     
-    # Check CSF TZ Settings
-    override_sales_invoice_qty = frappe.db.get_single_value("CSF TZ Settings", "override_sales_invoice_qty")
-    
     # Call the original get_item_details method
     response = get_item_details(args, doc, for_validate, overwrite_warehouse)
+
+    # Check CSF TZ Settings
+    override_sales_invoice_qty = frappe.db.get_single_value("CSF TZ Settings", "override_sales_invoice_qty")
 
     # If the setting is enabled, remove the qty field from the response
     if override_sales_invoice_qty:
